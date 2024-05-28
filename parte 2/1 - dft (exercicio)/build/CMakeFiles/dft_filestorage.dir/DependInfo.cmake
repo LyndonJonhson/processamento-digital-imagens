d332789bce38454c9de579db0fbfd36d1d5d1f93
lyndon.jonhson.700 <lyndon.jonhson.700@ufrn.edu.br>
2024-05-28 18:16:32 -0300

    adiciona parte 2.1 e 2.2

---
+++ parte 2/1 - dft (exercicio)/build/CMakeFiles/dft_filestorage.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jonhson/Documentos/UFRN/processamento-digital-imagens/parte 2/1 - dft (exercicio)/dft_filestorage.cpp" "CMakeFiles/dft_filestorage.dir/dft_filestorage.cpp.o" "gcc" "CMakeFiles/dft_filestorage.dir/dft_filestorage.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ parte 2/1 - dft (exercicio)/build/CMakeFiles/dft_filestorage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dft_filestorage.dir/dft_filestorage.cpp.o"
+  "CMakeFiles/dft_filestorage.dir/dft_filestorage.cpp.o.d"
+  "dft_filestorage"
+  "dft_filestorage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dft_filestorage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
